966185dccd8b9f98511b095b214c53c25d456769
shubham kumar <suraj2002fake@gmail.com>
2025-04-27 21:41:01 +0530

    professional experience and education completed

--- src/app/components/pages/about/education-summery/education-summery.component.ts
+++ src/app/components/pages/about/education-summery/education-summery.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { EducationService } from 'src/app/services/education.service';
+
+interface Education {
+  id: number;
+  degree: string;
+  institution: string;
+  field_of_study: string;
+  start_date: string;
+  end_date: string | null;
+  currently_studying: string;
+  grade: string;
+  description: string;
+  user_id: number;
+  created_at: string;
+  updated_at: string;
+}
 
 @Component({
   selector: 'app-education-summery',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './education-summery.component.html',
   styleUrl: './education-summery.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class EducationSummeryComponent {
+  educations: Education[] = [];
+
+  constructor(protected educationService: EducationService){}
+
+  ngOnInit(): void {
+    this.loadEducations();
+  }
+
+  loadEducations(){
+    this.educationService.getAll().subscribe((response) =>{
+      this.educations = response;
+    });
+  }
 
 }
+
--- src/app/components/pages/about/professional-experience/professional-experience.component.ts
+++ src/app/components/pages/about/professional-experience/professional-experience.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ProfessionalExperienceService } from 'src/app/services/professional-experience.service';
+
+interface Experience {
+  id: number;
+  user_id: number;
+  job_title: string;
+  company: string;
+  location: string;
+  start_date: string;
+  end_date: string | null;
+  currently_working: number;
+  description: string;
+  technologies: string[];
+}
+
 
 @Component({
   selector: 'app-professional-experience',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './professional-experience.component.html',
   styleUrl: './professional-experience.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ProfessionalExperienceComponent {
+  experiences: Experience[] = [];
+
+
+  constructor(public experience: ProfessionalExperienceService){
+
+  }
+
+  ngOnInit(): void {
+    this.loadProfressionalExperience();
+  }
+
+  loadProfressionalExperience(){
+    this.experience.getAll().subscribe((response) => {
+      this.experiences = response;
+    });
+  }
 
 }
